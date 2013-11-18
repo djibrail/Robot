88418a65da6d0379a7e881d012c332c201e7d91a
gab <sakho.gab@gmail.com>
2013-11-18 12:51:36 +0100

    fdkug

---
+++ EtatFige.cpp
@@ -0,0 +1,18 @@
+/*
+ * EtatFige.cpp
+ *
+ *  Created on: 18 nov. 2013
+ *      Author: user
+ */
+
+#include "EtatFige.h"
+
+EtatFige::EtatFige() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EtatFige::~EtatFige() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ EtatFige.h
@@ -0,0 +1,20 @@
+/*
+ * EtatFige.h
+ *
+ *  Created on: 18 nov. 2013
+ *      Author: user
+ */
+
+#ifndef ETATFIGE_H_
+#define ETATFIGE_H_
+
+class EtatFige {
+public:
+	EtatFige();
+	virtual ~EtatFige();
+
+	void repartir();
+
+};
+
+#endif /* ETATFIGE_H_ */
