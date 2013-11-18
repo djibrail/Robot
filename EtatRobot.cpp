b4bc7e58c46f5d6235d195e0982325f4a81c5a90
gab <sakho.gab@gmail.com>
2013-11-18 11:32:53 +0100

    firt one

---
+++ EtatRobot.cpp
@@ -0,0 +1,18 @@
+/*
+ * EtatRobot.cpp
+ *
+ *  Created on: 15 nov. 2013
+ *      Author: user
+ */
+
+#include "EtatRobot.h"
+
+EtatRobot::EtatRobot() {
+	// TODO Auto-generated constructor stub
+
+}
+
+EtatRobot::~EtatRobot() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ EtatRobot.h
@@ -0,0 +1,17 @@
+/*
+ * EtatRobot.h
+ *
+ *  Created on: 15 nov. 2013
+ *      Author: user
+ */
+
+#ifndef ETATROBOT_H_
+#define ETATROBOT_H_
+
+class EtatRobot {
+public:
+	EtatRobot();
+	virtual ~EtatRobot();
+};
+
+#endif /* ETATROBOT_H_ */
